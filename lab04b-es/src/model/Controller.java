f5aa5a29899dd0f8a8cb3aefd1c7d85c2ee9e03b
Matteo Di Noia <dmatteo002@gmail.com>
2023-10-30 09:21:06 +0100

    adding comment

--- lab04b-es/src/model/Controller.java
+++ lab04b-es/src/model/Controller.java
@@ -5,7 +5,7 @@ import java.awt.event.ActionListener;
 import java.util.Observable;
 
 public class Controller extends Observable implements ActionListener {
-
+    //Could have used a final model here (is better)
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
         if(actionEvent.getActionCommand().equals("generate")){
